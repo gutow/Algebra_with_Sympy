15afa44aa9331f5df446dbbe6136df9e987550cf
gutow <gutow@uwosh.edu>
2020-06-04 11:26:15 -0500

    PEP8 fixes

--- algebraic_equation.py
+++ algebraic_equation.py
@@ -18,7 +18,8 @@ This class should not be confused with the Boolean class ``Equality`` (abbreviat
 that the equality of two expressions is ``True``.
 
 This tool applies operations to both sides of the equation simultaneously, just as students are taught to do when 
-attempting to isolate (solve for) a variable. Thus the statement ``Equation/b`` yields a new equation ``Equation.lhs/b = Equation.rhs/b``
+attempting to isolate (solve for) a variable. Thus the statement ``Equation/b`` yields a new equation
+``Equation.lhs/b = Equation.rhs/b``
 
 The intent is to allow using the mathematical tools in SymPy to rearrange equations and perform algebra
 in a stepwise fashion. In this way more people can successfully perform algebraic rearrangements without stumbling
@@ -26,36 +27,26 @@ over missed details such as a negative sign. This mimics the capabilities availa
 (https://www.sagemath.org/) and [Maxima](http://maxima.sourceforge.net/), but can be installed in a generic python
 environment.
 
-_Setup/Installation_: Currently this tool is not available as a pip installable package. The file ``algebraic_equation.py``
-must be available for import in the directory space of the active Python, IPython or Jupyter notebook. To activate issue
-the command: ``from algebraic_equation import *``. This will also import the SymPy tools. If you want to isolate this tool
-from the global namespace you are working with change the import statement to ``import algebraic_equation as spa``, where 
-``spa`` stands for "SymPy Algebra". Then all calls would be made to ``spa.funcname()``.
+_Setup/Installation_: Currently this tool is not available as a pip installable package. The file
+``algebraic_equation.py`` must be available for import in the directory space of the active Python, IPython or Jupyter
+notebook. To activate issue the command: ``from algebraic_equation import *``. This will also import the SymPy tools.
+If you want to isolate this tool from the global namespace you are working with change the import statement to ``import
+algebraic_equation as spa``, where ``spa`` stands for "SymPy Algebra". Then all calls would be made to
+``spa.funcname()``.
 
-Usage examples can be found in the docstrings and the demonstration Jupyter notebook ``Demonstration of algebraic_equation.py.ipynb``.
+Usage examples can be found in the docstrings and the demonstration Jupyter notebook
+``Demonstration of algebraic_equation.py.ipynb``.
 
 J.G 28-5-20
 """
-'''
-Selective import kept for incorporation into SymPy
-from sympy.core.basic import Basic
-'''
 from sympy.core.sympify import _sympify
-'''
-from sympy.simplify import simplify
-from sympy.core.relational import Equality
-from sympy.core import Function
-from sympy.functions import *
-from sympy import functions
-from sympy import factorial
-from sympy import latex
-'''
+from sympy import *  # Replace with more selective import when folded into SymPy
 
-from sympy import * # Replace with more selective import when folded into SymPy
 
 class Equation(Basic):
     """
-    This class defines an equation with a left-hand-side (lhs) and a right-hand-side (rhs) connected by an operator (e.g. $p*V = n*R*T$).
+    This class defines an equation with a left-hand-side (lhs) and a right-hand-side (rhs) connected by an operator
+    (e.g. $p*V = n*R*T$).
     
     Explanation
     ===========
@@ -74,10 +65,11 @@ class Equation(Basic):
     
     Examples
     ========
-    >>> var('a b c')
-    >>> equ(a,b/c)
+    >>> from algebraic_equation import *
+    >>> a,b,c = var('a b c')
+    >>> Eqn(a,b/c)
     a=b/c
-    >>> t=equ(a,b/c)
+    >>> t=Eqn(a,b/c)
     >>> t
     a=b/c
     >>> t*c
@@ -114,20 +106,23 @@ class Equation(Basic):
     """
 
     def __new__(cls, lhs, rhs, **kwargs):
-        relop = kwargs.pop('relop','=')
-        check = kwargs.pop('check',True)
-        if (relop != '='):
-           raise NotImplementedError('"=" is the only relational operator presently supported in Equations.')
-        cls.relop=relop
+        relop = kwargs.pop('relop', '=')
+        check = kwargs.pop('check', True)
+        if relop != '=':
+            raise NotImplementedError('"=" is the only relational operator presently supported in Equations.')
+        cls.relop = relop
         lhs = _sympify(lhs)
         rhs = _sympify(rhs)
         if check:
             lsimp = lhs.simplify()
             rsimp = rhs.simplify()
-            if (lhs.is_number) and (rhs.is_number) and (lsimp != rsimp):
+            if lhs.is_number and rhs.is_number and lsimp != rsimp:
                 from warnings import warn
-                warn('Did your really mean to define unequal numbers as equal? ' + str(lsimp) +'='+ str(rsimp))
-        return super().__new__(cls, lhs, rhs, **kwargs)
+                warn('Did your really mean to define unequal numbers ' + str(lsimp) + ' and ' + str(
+                    rsimp) +
+                     ' as equal? To suppress this warning include `check=False` in the equation definition: ' +
+                     '`Eqn(lhs,rhs,check=False)`.')
+        return super().__new__(cls, lhs, rhs)
 
     @property
     def lhs(self):
@@ -142,7 +137,7 @@ class Equation(Basic):
         Returns the rhs of the equation.
         """
         return self.args[1]
-    
+
     def as_Boolean(self):
         """
         Converts the equation to an Equality.
@@ -162,22 +157,23 @@ class Equation(Basic):
         # has the attribute we will try to apply it to both. This will raise an error if both sides do
         # not support the operation.
 
-        side=kwargs.pop('Eqn_apply_side',None)
-        if (side=='both'):
-            if (hasattr(self.lhs,str(func))) or (hasattr(self.rhs,str(func))):
-                return Equation(getattr(self.lhs,str(func))(*args, **kwargs),getattr(self.rhs,str(func))(*args, **kwargs), check=False)
+        side = kwargs.pop('Eqn_apply_side', None)
+        if side == 'both':
+            if (hasattr(self.lhs, str(func))) or (hasattr(self.rhs, str(func))):
+                return Equation(getattr(self.lhs, str(func))(*args, **kwargs),
+                                getattr(self.rhs, str(func))(*args, **kwargs), check=False)
             else:
                 return Equation(func(self.lhs, *args, **kwargs), func(self.rhs, *args, **kwargs), check=False)
-        elif (side == 'lhs'):
-            if (hasattr(self.lhs,str(func))):
-                return Equation(getattr(self.lhs,str(func))(*args, **kwargs),self.rhs, check=False)
+        elif side == 'lhs':
+            if hasattr(self.lhs, str(func)):
+                return Equation(getattr(self.lhs, str(func))(*args, **kwargs), self.rhs, check=False)
             else:
                 return Equation(func(self.lhs, *args, **kwargs), self.rhs, check=False)
-        elif (side == 'rhs'):
-            if (hasattr(self.rhs,str(func))):
-                return Equation(self.lhs, getattr(self.rhs,str(func))(*args, **kwargs),check=False)
+        elif side == 'rhs':
+            if hasattr(self.rhs, str(func)):
+                return Equation(self.lhs, getattr(self.rhs, str(func))(*args, **kwargs), check=False)
             else:
-                return Equation(self.lhs, func(self.rhs, *args, **kwargs),check=False)
+                return Equation(self.lhs, func(self.rhs, *args, **kwargs), check=False)
         else:
             raise ValueError('keyword `Eqn_apply_side` must be one of "both", "lhs" or "rhs".')
 
@@ -187,7 +183,7 @@ class Equation(Basic):
         instead of the global function. The operation is applied to both sides.
         """
         return self._applyfunc(func, *args, **kwargs, Eqn_apply_side='both')
-    
+
     def applylhs(self, func, *args, **kwargs):
         """
         If lhs side of the equation has a defined subfunction (attribute) of name ``func``, that will be applied
@@ -202,20 +198,21 @@ class Equation(Basic):
         """
         return self._applyfunc(func, *args, **kwargs, Eqn_apply_side='rhs')
 
-#####
-# Overrides of binary math operations
-#####
+    #####
+    # Overrides of binary math operations
+    #####
 
-    _op_priority = 11.0 # This makes sure the rules for equations are applied before those for expressions
-                        # which have _op_priority = 10.0
+    _op_priority = 11.0  # This makes sure the rules for equations are applied before those for expressions
+
+    # which have _op_priority = 10.0
     @classmethod
     def _binary_op(cls, a, b, opfunc_ab):
         if isinstance(a, Equation) and not isinstance(b, Equation):
-            return Equation(opfunc_ab(a.lhs, b), opfunc_ab(a.rhs, b),check=False)
+            return Equation(opfunc_ab(a.lhs, b), opfunc_ab(a.rhs, b), check=False)
         elif isinstance(b, Equation) and not isinstance(a, Equation):
-            return Equation(opfunc_ab(a, b.lhs), opfunc_ab(a, b.rhs),check=False)
+            return Equation(opfunc_ab(a, b.lhs), opfunc_ab(a, b.rhs), check=False)
         elif isinstance(a, Equation) and isinstance(b, Equation):
-            return Equation(opfunc_ab(a.lhs, b.lhs), opfunc_ab(a.rhs, b.rhs),check=False)
+            return Equation(opfunc_ab(a.lhs, b.lhs), opfunc_ab(a.rhs, b.rhs), check=False)
         else:
             raise TypeError('One of a or b should be an equation')
 
@@ -246,7 +243,7 @@ class Equation(Basic):
     def __mod__(self, other):
         return self._binary_op(self, other, lambda a, b: a % b)
 
-    def __rmod__(self,other):
+    def __rmod__(self, other):
         return self._binary_op(other, self, lambda a, b: a % b)
 
     def __pow__(self, other):
@@ -257,94 +254,97 @@ class Equation(Basic):
 
     def _eval_power(self, other):
         return self.__pow__(other)
-    
-#####
-# Output helper functions
-#####
-    def __repr__(self):
-        return(str(self.lhs)+self.relop+str(self.rhs))
 
-    def _latex(self,obj,**kwargs):
-        return(latex(self.lhs)+self.relop+latex(self.rhs))
+    #####
+    # Output helper functions
+    #####
+    def __repr__(self):
+        return str(self.lhs) + self.relop + str(self.rhs)
+
+    def _latex(self, obj, **kwargs):
+        return latex(self.lhs, **kwargs) + self.relop + latex(self.rhs, **kwargs)
 
     def __str__(self):
-        return(self.__repr__())
+        return self.__repr__()
 
-#####
-# Operation helper functions
-#####
+    #####
+    # Operation helper functions
+    #####
     def expand(self, *args, **kwargs):
-        return Equation(self.lhs.expand(*args, **kwargs),self.rhs.expand(*args, **kwargs),check=False)
+        return Equation(self.lhs.expand(*args, **kwargs), self.rhs.expand(*args, **kwargs), check=False)
 
     def simplify(self, *args, **kwargs):
         return self._eval_simplify(*args, **kwargs)
 
     def _eval_simplify(self, *args, **kwargs):
-        return Equation(self.lhs.simplify(*args, **kwargs),self.rhs.simplify(*args, **kwargs),check=False)
+        return Equation(self.lhs.simplify(*args, **kwargs), self.rhs.simplify(*args, **kwargs), check=False)
 
     def _eval_factor(self, *args, **kwargs):
         # TODO: cancel out factors common to both sides.
-        return Equation(self.lhs.factor(*args, **kwargs),self.rhs.factor(*args, **kwargs),check=False)
+        return Equation(self.lhs.factor(*args, **kwargs), self.rhs.factor(*args, **kwargs), check=False)
 
     def factor(self, *args, **kwargs):
         return self._eval_factor(*args, **kwargs)
 
     def _eval_collect(self, *args, **kwargs):
-        return Equation(collect(self.lhs, *args, **kwargs),collect(self.rhs, *args, **kwargs),check=False)
+        return Equation(collect(self.lhs, *args, **kwargs), collect(self.rhs, *args, **kwargs), check=False)
 
     def collect(self, *args, **kwargs):
         return self._eval_collect(*args, **kwargs)
 
     def evalf(self, *args, **kwargs):
-        return Equation(self.lhs.evalf(*args, **kwargs),self.rhs.evalf(*args, **kwargs),check=False)
+        return Equation(self.lhs.evalf(*args, **kwargs), self.rhs.evalf(*args, **kwargs), check=False)
 
     def _eval_derivative(self, *args, **kwargs):
         # TODO Find why diff and Derivative do not appear to pass through kwargs to this.
         # Since we cannot set evaluation of lhs manually try to be intelligent about when
         # to do it.
         eval_lhs = False
-        if not(isinstance(self.lhs,Derivative)):
+        if not (isinstance(self.lhs, Derivative)):
             for sym in args:
-                if sym in self.lhs.free_symbols and not(_sympify(sym).is_number):
-                    eval_lhs=True
-        return Equation(self.lhs.diff(*args, **kwargs, evaluate=eval_lhs), self.rhs.diff(*args, **kwargs),check=False)
+                if sym in self.lhs.free_symbols and not _sympify(sym).is_number:
+                    eval_lhs = True
+        return Equation(self.lhs.diff(*args, **kwargs, evaluate=eval_lhs), self.rhs.diff(*args, **kwargs), check=False)
 
     def integ(self, *args, **kwargs):
         """
         This function is a convenience function that returns a new equation consisting of an unevaluated
         integral of the lhs as the new lhs and the result of integrating the rhs as the new rhs.
         """
-        return Equation(Integral(self.lhs, *args, **kwargs), self.rhs.integrate(*args, **kwargs),check=False)
+        return Equation(Integral(self.lhs, *args, **kwargs), self.rhs.integrate(*args, **kwargs), check=False)
 
     def _eval_Integral(self, *args, **kwargs):
-        side = kwargs.pop('side',None) # Could not seem to pass values for `evaluate` through to here.
-        if (side is None):
+        side = kwargs.pop('side', None)  # Could not seem to pass values for `evaluate` through to here.
+        if side is None:
             raise ValueError('You must specify `side="lhs"` or `side="rhs"` when integrating an Equation')
         else:
             try:
-                return (getattr(self,side).integrate(*args, **kwargs))
+                return (getattr(self, side).integrate(*args, **kwargs))
             except AttributeError:
                 raise AttributeError('`side` must equal "lhs" or "rhs".')
 
+
 Eqn = Equation
+
 
 #####
 # Extension of the Function class. For incorporation into SymPy this should become part of the class
 #####
 class Function(Function):
     def __new__(cls, *arg, **kwargs):
-        if (hasattr(arg[0],'applyfunc')):
-            return(arg[0].applyfunc(cls,*arg[1:],**kwargs))
+        if hasattr(arg[0], 'applyfunc'):
+            return arg[0].applyfunc(cls, *arg[1:], **kwargs)
         else:
-            return(super().__new__(cls, *arg, **kwargs))
+            return super().__new__(cls, *arg, **kwargs)
 
-for func in functions.__all__: # TODO: This will not be needed when incorporated into SymPy
+
+for func in functions.__all__:  # TODO: This will not be needed when incorporated into SymPy
     # listed in `skip` cannot be extended because of `mro` error or `metaclass conflict`. Seems to reflect
     #   expectation that a helper function will be defined within the object (e.g. `_eval_power()` for
     #   all the flavors of `root`).
-    skip=('sqrt','root','Min','Max','Id','real_root','cbrt','unbranched_argument','polarify','unpolarify',
-         'piecewise_fold','E1','Eijk','bspline_basis','bspline_basis_set','interpolating_spline','jn_zeros',
-          'jacobi_normalized','Ynm_c')
+    skip = ('sqrt', 'root', 'Min', 'Max', 'Id', 'real_root', 'cbrt', 'unbranched_argument', 'polarify', 'unpolarify',
+            'piecewise_fold', 'E1', 'Eijk', 'bspline_basis', 'bspline_basis_set', 'interpolating_spline', 'jn_zeros',
+            'jacobi_normalized', 'Ynm_c')
     if func not in skip:
-        execstr = 'class '+str(func)+'('+str(func)+',Function):\n    pass\n'
-        exec(execstr,globals(),locals())
+        execstr = 'class ' + str(func) + '(' + str(func) + ',Function):\n    pass\n'
+        exec(execstr, globals(), locals())
