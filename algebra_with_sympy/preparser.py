fed653fd123fc5f90600f4a779282a6b9b0c70b3
Jonathan Gutow <gutow@uwosh.edu>
2022-04-25 20:16:54 -0500

    Preparser for shortened input instead of magic

--- algebra_with_sympy/preparser.py
+++ algebra_with_sympy/preparser.py
@@ -9,10 +9,15 @@ def algebra_with_sympy_preparser(lines):
     and `equation.rhs` is the right-hand side of the equation. Each side of the
     equation must parse into a valid Sympy expression.
 
-    If the `equation_name` is omitted the equation will be formed, but it will
+    **Note**: If the `equation_name` is omitted the equation will be formed,
+    but it will
     not be assigned to a name that can be used to refer to it later. You may be
     able to access it through one of the special IPython underscore names. This
     is not recommended.
+
+    **THIS FUNCTION IS USED BY THE IPYTHON ENVIRONMENT TO PREPARSE THE INPUT
+    BEFORE IT IS PASSED TO THE PYTHON INTERPRETER. IT IS NOT MEANT TO BE USED
+    DIRECTLY BY A USER**
     """
     new_lines = []
     for k in lines:
